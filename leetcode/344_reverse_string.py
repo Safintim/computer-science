253f49b06c94d36766ee99824b02b0988919603e
Safintim <timurtlt96@mail.ru>
2021-05-20 11:40:46 +0300

    add 344 reverse_string

---
+++ leetcode/344_reverse_string.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def reverse_string(s: List[str]) -> None:
+    start = 0
+    end = len(s) - 1
+    while start < end:
+        print(start, end)
+        s[start], s[end] = s[end], s[start]
+        start += 1
+        end -= 1
