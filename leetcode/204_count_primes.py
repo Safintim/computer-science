19fe6dc279bf4db4186545e3e2ea055ce2039b46
Safintim <timurtlt96@mail.ru>
2021-05-20 16:38:04 +0300

    Add 204_count_primes

---
+++ leetcode/204_count_primes.py
@@ -0,0 +1,10 @@
+def count_primes(self, n: int) -> int:
+    if n <= 2:
+        return 0
+
+    numbers = {}
+    for i in range(2, int(n ** 0.5) + 1):
+        if i not in numbers:
+            for j in range(i * i, n, i):
+                numbers[j] = False
+    return n - len(numbers) - 2
